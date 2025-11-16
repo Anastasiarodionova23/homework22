5c36f57cf1cce93d083975aa897bed65989e27fa
ansty <anastasiarodionova086@gmail.com>
2025-11-17 00:28:40 +0300

    Changing the file name

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,14 +1,12 @@
 package main.java.com;
 
-import java.sql.SQLOutput;
-
-public class main {
+public class Main {
     public static void main(String[] args) {
         int priceTicket = 13676; // билет в рублях
         int rublOnMile = 20; // количество рублей для 1 мили
         int bonus = priceTicket / rublOnMile; // расчет миль
 
         System.out.println("За покупку билета стоимостью " + priceTicket + " рублей");
-        System.out.println("начислено " + bonus + " бонусных миль");
+        System.out.println("Начислено " + bonus + " бонусных миль");
     }
 }
